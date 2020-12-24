ec9bfe086e28390bde7e93dca49e4034bcd2d963
CreativeMetalCat <catguy228@gmail.com>
2020-12-24 11:08:20 +1000

    Added sounds

--- Entities/Player.gd
+++ Entities/Player.gd
@@ -4,13 +4,18 @@ export var Limit: float
 
 export var Speed = 100
 
+var playerSprite:AnimatedSprite;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	playerSprite = get_node("AnimatedSprite");
 	pass  # Replace with function body.
 
 # Call this when player dies
 func _die():
+	if(playerSprite != null):
+		playerSprite.animation = "Explosion";
 	pass
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
